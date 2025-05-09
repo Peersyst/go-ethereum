33a6ad207fdc71d9b924f22e3c90a06904eb0447
AdriaCarrera <adria.cm95@gmail.com>
2025-05-09 17:22:17 +0200

    feat: add header from RPC

--- core/types/block.go
+++ core/types/block.go
@@ -63,6 +63,7 @@ func (n *BlockNonce) UnmarshalText(input []byte) error {
 
 // Header represents a block header in the Ethereum blockchain.
 type Header struct {
+	RpcHash     common.Hash    `json:"hash"             gencodec:"required"`
 	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
 	UncleHash   common.Hash    `json:"sha3Uncles"       gencodec:"required"`
 	Coinbase    common.Address `json:"miner"`
